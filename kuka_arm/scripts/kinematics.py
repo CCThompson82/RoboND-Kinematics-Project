57c4a9d7aa3f9f339fa57da758ae4d46a26582d0
CCThompson82 <christhompson@xihelm.com>
2018-01-03 07:26:19 +0000

    Feat: Change variable names to reflect Z versus Y component of B

--- kuka_arm/scripts/kinematics.py
+++ kuka_arm/scripts/kinematics.py
@@ -23,19 +23,18 @@ class Solver(object):
 
     def solve_IK(self):
         """
-
+        Calculates IK solution(s) to the target pose setup during the object
+        initiation
         """
         theta1 = self.solve_theta1()
         theta2, theta3 = self.solve_theta23()
         theta4, theta5, theta6 = self.solve_theta456(
             theta1=theta1, theta2=theta2, theta3=theta3)
         entry1 = [theta1, theta2, theta3, theta4, theta5, theta6]
-        # for idx, q in enumerate(entry1):
-        #     if q > float(pi):
-        #         entry1[idx] = 0 - (q % pi)
-        #     elif q < float(-pi):
-        #         entry1[idx] = 0 - (q % pi)
         solution_set = [entry1]
+
+        # TODO: calculate further solutions to the target IK problem and append to solution_set
+
         return solution_set
 
 
@@ -53,17 +52,17 @@ class Solver(object):
         A = self.dhp.DH[self.dhp.d4]
         C = self.dhp.DH[self.dhp.a2]
 
-        By = self.WC[2] - self.dhp.DH[self.dhp.d1]
+        Bz = self.WC[2] - self.dhp.DH[self.dhp.d1]
         Bx = sqrt((self.WC[0])**2 + (self.WC[1])**2) - self.dhp.DH[self.dhp.a1]
 
-        B = sqrt((Bx)**2 + (By)**2)
+        B = sqrt((Bx)**2 + (Bz)**2)
 
         angle_a = acos(((B)**2 + (C)**2 - (A)**2) / (2*B*C))
         angle_b = acos(((A)**2 + (C)**2 - (B)**2) / (2*A*C))
         angle_c = acos(((A)**2 + (B)**2 - (C)**2) / (2*A*B))
 
-        theta2 = ((pi/2) - angle_a - atan2(By, Bx)).evalf()
-        theta3 = float(pi/2 - (angle_b + 0.036))
+        theta2 = ((pi/2) - angle_a - atan2(Bz, Bx)).evalf()
+        theta3 = float(pi/2 - (angle_b + 0.036)) #TODO: explicit sag angle calc
         return theta2, theta3
 
 
